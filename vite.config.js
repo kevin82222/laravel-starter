d69435a5f5a62a5d31a9d6e5a21a6d6097aeaeda
Kevin Lin <kevin82222@gmail.com>
2023-04-08 02:57:07 +0800

    auto import vue functions and components

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import autoImport from 'unplugin-auto-import/vite'
+import components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
   plugins: [
+    autoImport({
+      imports: [
+        'vue',
+        {
+          '@inertiajs/vue3': ['router', 'useForm', 'usePage', 'useRemember'],
+        },
+      ],
+      dts: 'resources/js/types/auto-imports.d.ts',
+    }),
+    components({
+      dirs: [
+        'resources/js/Components',
+        'resources/js/Layouts',
+      ],
+      dts: 'resources/js/types/components.d.ts',
+      resolvers: [
+        (name) => {
+          const components = ['Head', 'Link']
+          if (components.includes(name))
+            return { name, from: '@inertiajs/vue3' }
+        },
+      ],
+    }),
     laravel({
       input: 'resources/js/app.ts',
       refresh: true,
